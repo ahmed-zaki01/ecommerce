af642c13d26a452d3f028e6a3115c5c35ef89b44
ahmed_zaki0263 <ahmed.zaki0263@gmail.com>
2020-07-09 18:17:02 +0200

    add adminLte theme to project

---
+++ resources/views/dashboard/layout.blade.php
@@ -0,0 +1,10 @@
+@include('dashboard.inc.header')
+@include('dashboard.inc.aside')
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    @yield('content')
+</div>
+<!-- /.content-wrapper -->
+
+@include('dashboard.inc.footer')
